c5e1e8aeb8a50bc9c3ccd3fc9e5a3edddef4dfa7
Ciaran <ciaran_r@berkeley.edu>
2020-04-10 15:18:14 -0700

    Adding refernce frame conversion to code

--- dev_files/init_cond/Twogen2bus.m
+++ dev_files/init_cond/Twogen2bus.m
@@ -3,6 +3,7 @@ function mpc = Twogen2bus
 
 %% system MVA base
 mpc.baseMVA = 1000;
+mpc.version = '2';
 
 %% bus data
 %	bus_i	type	Pd	Qd	Gs	Bs	area	Vm	Va	baseKV	zone	Vmax	Vmin
@@ -14,12 +15,12 @@ mpc.bus = [
 %% generator data
 %	bus	Pg	Qg	Qmax	Qmin	Vg	mBase	status	Pmax	Pmin	Pc1	Pc2	Qc1min	Qc1max	Qc2min	Qc2max	ramp_agc	ramp_10	ramp_30	ramp_q	apf
 mpc.gen = [
-	1	700	70	100	-100	1.02	1000	1000	50	0	0	0	0	0	0	0	0	0	0	0;
+	1	700	70	100	-100	1.02	1000	1000	1000	0	0	0	0	0	0	0	0	0	0	0;
 	%2	50 -30	100	-100	1.0	100	1	150	37.5	0	0	0	0	0	0	0	0	0	0	0;
 ];
 
 %% branch data
 %	fbus	tbus	r	x	b	rateA	rateB	rateC	ratio	angle	status	angmin	angmax
 mpc.branch = [
-	1	2	0.0097	0.097	0.00000306933	100	100	100	0	0	1	-1.04	1.04;	
+	1	2	0.065	 0.65	 0.0438	100	100	100	0	0	1	-1.04	1.04;	
 ];
